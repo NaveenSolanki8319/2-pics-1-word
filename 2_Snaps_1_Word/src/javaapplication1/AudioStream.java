6bca1dd97c5e68a89d7ce11eaf58e7e16ef7b99c
Naveen Solanki <100820347+NaveenSolanki8319@users.noreply.github.com>
2022-06-18 19:02:19 +0530

    first commit

---
+++ 2_Snaps_1_Word/src/javaapplication1/AudioStream.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication1;
+
+import java.io.InputStream;
+
+/**
+ *
+ * @author Naveen
+ */
+public class AudioStream {
+
+    public AudioStream(InputStream music) {
+    }
+    
+}
